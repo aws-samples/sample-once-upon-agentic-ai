a7eeba3273907e204955a7bfb056eb6106d5d2ea
Tagazok <leplus.olivier@gmail.com>
2026-02-24 20:38:44 +0100

    fi(baseurl): Fix baseurl

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/',
+  base: '/sample-once-upon-agentic-ai/',
   plugins: [vue()],
   resolve: {
     alias: {
